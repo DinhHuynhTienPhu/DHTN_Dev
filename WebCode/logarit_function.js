00b64a4f1557ffff4fd9e3aed2ba5e82f31b7379
DinhHuynhTienPhu <40557558+DinhHuynhTienPhu@users.noreply.github.com>
2020-08-16 21:29:44 +0700

    Update logarit_function.js
    Now user can input base and value want to calculate

--- WebCode/logarit_function.js
+++ WebCode/logarit_function.js
@@ -4,3 +4,9 @@ function getBaseLog(x, y) {
 }
 
 alert("log2(5)= "+getBaseLog(2, 5)); 
+alert("Now you can calculate yourself");
+while(true){
+var base = window.prompt("Please enter Log base: ");
+var value=window.prompt("Please enter Value you want to calculate: ");
+alert("log_" + base+"("+value+")="+getBaseLog(base,value));
+}
